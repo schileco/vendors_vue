c657191c28d105bad510df88811b40ad5dd3997c
vicgiupat <viktor.soares1989@gmail.com>
2023-10-17 16:26:45 -0300

    correct url now rsrs

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://https://vendor-schulze-k668oofao-schulzes-projects.vercel.app//'
+    baseURL:'http://https://vendor-schulze-k668oofao-schulzes-projects.vercel.app/'
 });
 
 export default api
